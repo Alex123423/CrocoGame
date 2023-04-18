92d18dd685c069fc01f786ae677b960c780ec838
vikharevam <ViharevAMIP@gmail.com>
2023-04-19 01:08:03 +0300

    Added constrats

--- CrocoGame/Controllers/GameViewController.swift
+++ CrocoGame/Controllers/GameViewController.swift
@@ -24,8 +24,18 @@ class GameViewController: UIViewController {
     ]
     
     
-    
     let verStack: UIStackView = {
+        let subStack = UIStackView()
+        subStack.axis = .vertical
+        subStack.alignment = .center
+        subStack.distribution = .equalSpacing
+        subStack.spacing = 56
+        subStack.translatesAutoresizingMaskIntoConstraints = false
+        return subStack
+    }()
+    
+    
+    let verStackTitle: UIStackView = {
         let subStack = UIStackView()
         subStack.axis = .vertical
         subStack.alignment = .center
@@ -35,16 +45,25 @@ class GameViewController: UIViewController {
         return subStack
     }()
     
+    
+    let verStackButton: UIStackView = {
+        let subStack = UIStackView()
+        subStack.axis = .vertical
+        subStack.alignment = .center
+        subStack.distribution = .equalSpacing
+        subStack.spacing = 11
+        subStack.translatesAutoresizingMaskIntoConstraints = false
+        return subStack
+    }()
+    
+    
+    
     let imageBackground: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "background")
-        image.contentMode = .scaleToFill
-        image.clipsToBounds = false
-        image.translatesAutoresizingMaskIntoConstraints = false
         image.contentMode = .scaleAspectFit
-        
-        
-        
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
@@ -65,6 +84,7 @@ class GameViewController: UIViewController {
         label.text = "00:59"
         label.font = .systemFont(ofSize: 48)
         label.backgroundColor = .clear
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -74,8 +94,7 @@ class GameViewController: UIViewController {
         label.text = "Картошка"
         label.font = .systemFont(ofSize: 48)
         label.backgroundColor = .clear
-        
-    
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -86,26 +105,28 @@ class GameViewController: UIViewController {
         label.text = "объясни с помощью жестов"
         label.font = .systemFont(ofSize: 20)
         label.backgroundColor = .clear
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
-        
     }()
     
     
     
-    let buttonCorrect: UIButton = {
-        let button = UIButton()
+    lazy var buttonCorrect: UIButton = {
+        let button = UIButton(type: .system)
         button.setTitle("Правильно", for: .normal)
         button.setTitleColor(.white, for: .normal)
+        button.bounds.size.width = 300
         button.backgroundColor = .green
         button.layer.cornerRadius = 15
         button.titleLabel?.font = .systemFont(ofSize: 17.0, weight: .medium)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     
-    let buttonViolation: UIButton = {
-        let button = UIButton()
+    lazy var buttonViolation: UIButton = {
+        let button = UIButton(type: .system)
         button.setTitle("Нарушил правила", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .red
@@ -116,22 +137,25 @@ class GameViewController: UIViewController {
     }()
     
     
-    let buttonReset: UIButton = {
-        let button = UIButton()
+    lazy var buttonReset: UIButton = {
+        let button = UIButton(type: .system)
         button.setTitle("Сброс", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .gray
         button.layer.cornerRadius = 15
-        button.titleLabel?.font = .systemFont(ofSize: 17.0, weight: .medium)
+        button.titleLabel?.font = .systemFont(ofSize: 17.0, weight: .regular)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
-  
+    
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
-      
+        setupHierarchy()
+        setConstrains()
+        
     }
     
     
@@ -140,9 +164,23 @@ class GameViewController: UIViewController {
     }
     
     
-    
-    
     func setupHierarchy(){
+        
+        view.addSubview(imageBackground)
+        view.addSubview(wordsLabel)
+        view.addSubview(verStackButton)
+        view.addSubview(verStackTitle)
+        view.addSubview(verStack)
+        
+        verStack.addArrangedSubview(imageLitleCrocodile)
+        verStack.addArrangedSubview(timerLabel)
+        
+        verStackTitle.addArrangedSubview(wordsLabel)
+        verStackTitle.addArrangedSubview(conditionsLabel)
+        
+        verStackButton.addArrangedSubview(buttonCorrect)
+        verStackButton.addArrangedSubview(buttonViolation)
+        verStackButton.addArrangedSubview(buttonReset)
         
         
     }
@@ -150,10 +188,46 @@ class GameViewController: UIViewController {
     
     func setConstrains(){
         
+        NSLayoutConstraint.activate([
+            
+            verStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            verStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 36),
+            verStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 116),
+            verStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -116),
+            
+            
+            buttonCorrect.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            buttonCorrect.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            buttonViolation.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            buttonViolation.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            buttonReset.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            buttonReset.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            
+            
+            buttonCorrect.heightAnchor.constraint(equalToConstant: 60),
+            buttonViolation.heightAnchor.constraint(equalToConstant: 60),
+            buttonReset.heightAnchor.constraint(equalToConstant: 60),
+            
+            
+            
+            
+            verStackTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            verStackTitle.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            
+            verStackButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 54),
+            verStackButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -54),
+            verStackButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            verStackButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            
+        ])
+        
         
     }
     
-    
-    
-    
 }
+
+
+
+
+
